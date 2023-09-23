538e17cb2c2ea157fc397052be6a7dfb4be0aa22
Matthias Goebl <matthias.goebl@goebl.net>
2023-09-23 19:07:17 +0200

    migrate main playground to remote s3 state

--- provider.tf
+++ provider.tf
@@ -12,16 +12,15 @@ terraform {
   required_version = "~>1.4"
 }
 
-data "terraform_remote_state" "playground_setup" {
-  backend = "local"
-
-  config = {
-    path = "./setup-playground/terraform.tfstate"
-  }
-}
+# data "terraform_remote_state" "playground_setup" {
+#   backend = "local"
+#   config = {
+#     path = "./setup-playground/terraform.tfstate"
+#   }
+# }
 
 provider "aws" {
-  region     = data.terraform_remote_state.playground_setup.outputs.region
-  access_key = data.terraform_remote_state.playground_setup.outputs.playground_access_key
-  secret_key = data.terraform_remote_state.playground_setup.outputs.playground_secret_key
+  #   region     = data.terraform_remote_state.playground_setup.outputs.region
+  #   access_key = data.terraform_remote_state.playground_setup.outputs.playground_access_key
+  #   secret_key = data.terraform_remote_state.playground_setup.outputs.playground_secret_key
 }
